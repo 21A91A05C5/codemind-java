1c20716adaf3b2299e242405330bc6b52268534e
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2023-02-05 09:52:08 +0530

    import java.util.*; class s{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         {             String a=sc.nextLine();             System.out.println(a);         }     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class s{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        {
+            String a=sc.nextLine();
+            System.out.println(a);
+        }
+    }
+}
