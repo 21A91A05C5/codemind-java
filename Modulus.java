d07c7b74baed9b0fde96145ca1c5f1ea14094f5b
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2023-02-05 09:56:46 +0530

    import java.util.*; class s {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         {             int a=sc.nextInt();             int b=sc.nextInt();             int res;             res=a%b;             System.out.println(res);         }     } }

---
+++ Modulus.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class s
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int res;
+            res=a%b;
+            System.out.println(res);
+        }
+    }
+}
